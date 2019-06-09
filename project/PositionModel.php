b3fcc2b8cb4a266f29c02d63fb43d117e9c5aac4
vararucristian <48356098+vararucristian@users.noreply.github.com>
2019-06-10 02:29:17 +0300

    add standart photos /sensor code

--- project/PositionModel.php
+++ project/PositionModel.php
@@ -1,9 +1,28 @@
 <?php
 class PositionModel{
     private $url;
+    private $host = "localhost";
+    private $user = "root";
+    private $password = "";
+    private $db = "project";
+    private $connection;
 function __construct($id){
-    $this->url="localhost/Kimo-TW-Project/project/sensorApi.php?id=".$id;
+    $this->connection= new mysqli($this->host, $this->user, $this->password, $this->db);
+    $sensorCode=$this->getSensorCode($id);
+    $this->url="localhost/Kimo-TW-Project/project/sensorApi.php?id=".$sensorCode;
 }
+
+private function getSensorCode($id){
+    $sql="select value from sensor_codes join kids_sensor on id_sensor=id where id_kid=?";
+    $stmt=$this->connection->prepare($sql);
+    $stmt->bind_param("i",$id);
+    $stmt->execute();
+    $stmt->bind_result($sensorCode);
+    $stmt->fetch();
+    return $sensorCode;
+}
+
+
 function getPosition(){
     $c = curl_init ();
 curl_setopt ($c, CURLOPT_URL, $this->url);              
